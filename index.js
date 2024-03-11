0a8997bb6bb8181629de1efe0ca929971de0e18b
thaytoanlaptrinh <thaytoanlaptrinh@gmail.com>
2024-03-11 10:08:02 +0700

    ipv6

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const express = require('express');
-const app = express();
-const port = 3000; // Chọn một cổng, ví dụ: 3000
+const { createServer } = require('http');
+const os = require('os');
 
-// Định nghĩa route cho trang chủ
+const app = express();
+
+const server = createServer(app);
+const port = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
-    res.send('Hello, this is your Express server!');
+    res.send('Hello World!');
 });
 
-// Lắng nghe các kết nối đến cổng đã chọn
-app.listen(port, () => {
-    console.log(`http://localhost:${port}`);
+server.listen(port, () => {
+    const ifaces = os.networkInterfaces();
+    let ipv6Address;
+
+    // Lặp qua tất cả các interface để tìm IPv6
+    Object.keys(ifaces).forEach((ifname) => {
+        ifaces[ifname].forEach((iface) => {
+            if (iface.family === 'IPv6' && !iface.internal) {
+                ipv6Address = iface.address;
+            }
+        });
+    });
+
+    console.log(`Server listening on http://[${ipv6Address}]:${port}`);
 });
